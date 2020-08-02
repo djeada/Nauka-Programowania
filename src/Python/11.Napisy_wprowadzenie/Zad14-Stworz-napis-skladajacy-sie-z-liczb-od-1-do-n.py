30eaaee7ac1402f4119cdc675a8bbb641c56a86c
djeada <addjellouli1@gmail.com>
2020-08-03 00:40:27 +0200

    dodano zadanie: stworz napis skladajacy sie z liczb od 1 do n.

---
+++ src/Python/11.Napisy_wprowadzenie/Zad14-Stworz-napis-skladajacy-sie-z-liczb-od-1-do-n.py
@@ -0,0 +1,25 @@
+'''
+Stworz napis skladajacy sie z liczb od 1 do n.	
+'''
+
+#Wersja 1
+def liczbyDoNV1(n):
+	return ''.join([str(i) for i in range(1, n + 1)])
+
+#Wersja 2
+def liczbyDoNV2(n):
+	
+	wynik = ''
+	
+	for i in range(n):
+		wynik += str(1 + i)
+
+	return wynik
+
+#Testy Poprawnosci
+assert(liczbyDoNV1(3) == '123')
+assert(liczbyDoNV2(3) == '123')
+
+assert(liczbyDoNV1(-1) == '')
+assert(liczbyDoNV2(-1) == '')
+
