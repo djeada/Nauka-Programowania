38372911a7875f25b41aab9b868c65f32047b7c2
djeada <addjellouli1@gmail.com>
2020-08-04 22:43:33 +0200

    Znajdz liczby podzielne przez x na przedziale [a, b].

---
+++ src/Python/04.Petla_wprowadzenie/Zad5.py
@@ -0,0 +1,15 @@
+'''
+Znajdz liczby podzielne przez x na przedziale [a, b].	
+'''
+
+print('Podaj liczby a, b oraz x: ')
+a = int(input())
+b = int(input())
+x = int(input())
+
+print('Liczby podzielne przez x na przedziale [a, b]: ')
+
+#Wersja 1
+for i in range(a, b + 1):
+	if i % x == 0:
+		print(i)
