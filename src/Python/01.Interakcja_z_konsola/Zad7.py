f84d454474961a105a3a8785ed845bb04c959f27
Mumbak <zinedin1000@gmail.com>
2020-08-08 17:53:17 +0200

    pola objetosci

---
+++ src/Python/01.Interakcja_z_konsola/Zad7.py
@@ -0,0 +1,10 @@
+'''
+Napisz program, ktory wczyta podstawe i wysokosc trojkata i wypisze jego pole. 
+''' 
+
+print("podaj podstawe i wysokosc trojkata")
+a = input()
+h = input()
+print("pole trojkata o podstawie", a , "i wyskosci", h , "jest rowne", a*h/2)
+
+
