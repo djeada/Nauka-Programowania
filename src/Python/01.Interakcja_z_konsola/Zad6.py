4d60765fbb2255ab72426cce2261d78d375194f3
Mumbak <zinedin1000@gmail.com>
2020-08-08 17:44:55 +0200

    jednostki

---
+++ src/Python/01.Interakcja_z_konsola/Zad6.py
@@ -0,0 +1,34 @@
+'''
+Napisz program, ktory wczyta wielkosc w kilogramach i wypisze ilu gramom odpowiada.
+kilograms = input("Wpisz kg:")
+'''
+print("podaj kg")
+kilogramy = input()
+gramy = kilogramy * 1000
+print(kilogramy, "kg to " , gramy, "g") 
+
+'''
+Napisz program, ktory wczyta wielkosc w calach i wypisze ilu centymetrom odpowiada.
+'''
+print("podaj cale")
+cal = input()
+cm = cal / 2.54
+print(cal, "cali to" , cm, "cm") 
+ 
+'''
+Napisz program, ktory wczyta ilosc sekund i przeliczy na godziny.
+'''
+
+'''
+Napisz program, ktory wczyta wielkosc w euro i wypisze ilu zlotowkom odpowiada. 
+'''
+
+'''
+Napisz program, ktory wczyta miare kata w stopniach i wypisze ilu radianom odpowiada. 
+'''
+
+'''
+ Napisz program, ktory wczyta temperature w stopniach Farenheita i wypisze ilu stopniom Celsjusza oraz ilu stopniom Kelwina odpowiada. 
+'''
+
+
