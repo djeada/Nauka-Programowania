d5a7f288b3f06efdf4290dea139703e54e3e0e08
Mumbak <zino@localhost.localdomain>
2020-08-08 17:16:35 +0200

    wartosc funkcji w punkcie

--- src/Python/01.Interakcja_z_konsola/Zad3.py
+++ src/Python/01.Interakcja_z_konsola/Zad3.py
@@ -10,9 +10,21 @@ print("xx")
 1
 22
 333
-
 '''
+
 print("1")
 print("22")
-print("333")
+print("333") 
+
+'''
+Romb z jedynek 
+
+'''
+
+print('  1')
+print(" 111")
+print("11111")
+print(" 111")
+print("  1") 
+
 
---
+++ src/Python/01.Interakcja_z_konsola/Zad5.py
@@ -0,0 +1,69 @@
+'''
+Napisz program, który wyznaczy wartość funkcji y = 3x + 10 w zadanym punkcie.
+'''
+x = input()
+print(3*x+10) 
+
+'''
+Napisz program, który wczyta współczynniki a, b oraz punkt x i wyznaczy wartość funkcji y = ax
++ b w zadanym punkcie.
+'''
+
+print("podaj a")
+a = input()
+print("podaj b")
+b = input() 
+print("podaj x")
+x = input() 
+print("a*x+b")
+print(a*x+b)
+
+'''
+Napisz program, który wyznaczy wartość funkcji y=x^3 + 2x^2 - 20 w zadanym punkcie.
+'''
+
+print("podaj x")
+x = input()
+print("y=x**3+2x**2-20")
+print(x**3+2*x**2-20)
+
+'''
+Napisz program, który wczyta współczynniki a, b, c, d, m, n oraz punkt x i wyznaczy wartość
+funkcji y = a*x^m + b*x^n + c - a
+'''
+
+print("wpisz a")
+a = input()
+print("wpisz b")
+b = input()
+print("wpisz c")
+c = input()
+print("wpisz d")
+d = input()
+print("wpisz m")
+m = input()
+print("wpisz n")
+n = input()
+print("wpisz x")
+x = input()
+print("a*x**m+b*x**n+c-a")
+print(a*x**m+b*x**n+c-a)
+
+'''
+Napisz program, który wyznaczy wartość funkcji y = sin3x*cos2x + e^x^2 + log(x^3+2x^2-x-3) w
+zadanym punkcie.
+'''
+
+import math
+print("napisz x")
+x = input()
+print("sin(3*x)*cos(2*X)+e**x**2+log(x**3+2*x**2-x-3")
+print(math.sin(3*x)*math.cos(2*x)+math.exp(x**2)+math.log(x**3+2*x**2-x-3))
+
+
+
+
+
+
+
+
