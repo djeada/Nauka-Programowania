7a7b7267c6e70fced3d4f9c87c38c4ae07f7d61c
djeada <addjellouli1@gmail.com>
2020-08-09 23:26:46 +0200

    porzadki

---
+++ src/Python/12.Napisy_wprowadzenie/Zad1.py
@@ -0,0 +1,22 @@
+'''
+Odwroc napis.
+'''
+
+#Wersja 1
+def odwrocNapisV1(napis):
+	return napis[::-1]
+
+#Wersja 2
+def odwrocNapisV2(napis):
+	if len(napis) <= 1:
+		return napis
+
+	return odwrocNapisV2(napis[1:]) + napis[0]
+
+#Testy Poprawnosci
+napis = 'abc'
+wynik = 'cba'
+
+assert(odwrocNapisV1(napis) == wynik)
+assert(odwrocNapisV2(napis) == wynik)
+
