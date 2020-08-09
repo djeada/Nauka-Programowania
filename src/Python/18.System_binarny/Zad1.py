ce714da3dd46743b6b2ba9d5a97e2df6a4569cca
djeada <addjellouli1@gmail.com>
2020-08-09 15:24:52 +0200

    konwersja bin dec

---
+++ src/Python/18.System_binarny/Zad1.py
@@ -0,0 +1,20 @@
+'''
+Zamien liczbe binarna na jej odpowiednik w systemie dziesietnym
+'''
+
+#Wersja 1
+def naDziesietnyV1(liczba):
+	wynik = 0
+	for i, v in enumerate(liczba[::-1]):
+		wynik += (2**int(i)) * int(v)
+
+	return wynik
+
+#Testy poprawnosci
+liczba = '0011'
+wynik = 3
+assert(naDziesietnyV1(liczba) == wynik)
+
+liczba = '1111100'
+wynik = 124
+assert(naDziesietnyV1(liczba) == wynik)
