9de13555947eb44c3d12cc7e59acc574cbea95de
djeada <addjellouli1@gmail.com>
2020-08-09 23:26:46 +0200

    porzadki

---
+++ src/Python/07.Petla_zagniezdzone/Zad6.py
@@ -0,0 +1,25 @@
+'''
+Wypisz za pomoca petli, litere Z zbudowana z literek 'x'.
+
+Przyklad dla wysokosci 5:
+*****
+   * 
+  *  
+ *   
+*****
+'''
+
+#Wersja 1
+def wypiszZV1(wysokosc):
+    for j in range(wysokosc):
+        for i in range(wysokosc):
+            if j == 0 or j == wysokosc - 1:
+                print('*', end='')       
+            elif i == wysokosc - j - 1:
+                print('*', end='')
+            else:
+                print(' ', end='')
+        print('')
+
+for i in range(1, 6):
+	wypiszZV1(i)
---
+++ src/Python/23.Klasy/Zad3.py
@@ -0,0 +1,15 @@
+'''
+Otrzymujesz liste wspolrzednych wierzcholkow dwoch prostokatow.
+Znajdz wspolrzedne prostokata powstalego z nalozenia sie dwoch innych prostokatpw.
+'''
+
+def Prostokat():
+	
+	def __init__(self, listaWspolrzednychWierzcholkow):
+		self.listaWspolrzednychWierzcholkow = listaWspolrzednychWierzcholkow
+		
+	
+	def wypisz(self):
+		print('Wierzcholki prostokata: ')
+		for punkt in listaWspolrzednychWierzcholkow:
+			print(punkt)
