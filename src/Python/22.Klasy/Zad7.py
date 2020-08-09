4e0f392b752ebb6e6d3d37feb779bb96a501b542
djeada <addjellouli1@gmail.com>
2020-08-09 14:42:34 +0200

    metoda statyczna

---
+++ src/Python/22.Klasy/Zad7.py
@@ -0,0 +1,21 @@
+'''
+Wypisz liczbę instancji klasy w programie.
+'''
+
+#Wersja 1
+class Klasa:
+   liczbaInstancji = 0
+
+   def __init__(self):
+     Klasa.liczbaInstancji += 1
+
+   def wyswietl():
+      print("Aktualna liczba instancji: ", Klasa.liczbaInstancji)
+   
+   wyswietl = staticmethod(wyswietl)
+   
+#Testy poprawnosci
+N = 5
+[Klasa() for i in range(N)]
+
+assert(Klasa.liczbaInstancji == N)
