c7eabe7e049d2beb50e22be43d338e62a6e12806
djeada <addjellouli1@gmail.com>
2020-08-09 15:12:22 +0200

    dziedziczenie

---
+++ src/Python/23.Dziedziczenie/Zad1.py
@@ -0,0 +1,22 @@
+'''
+Wywolaj metode klasy bazowej w metodzie klasy potomnej nadipsujacej ta metode.
+'''
+
+#Wersja 1
+class Rodzic():
+	def __init__(self):
+		print('Jestem rodzicem')
+
+class DzieckoA(Rodzic):
+	def __init__(self):
+		print('Jestem dzieckiem A')
+
+class DzieckoB(Rodzic):
+	def __init__(self):
+		print('Jestem dzieckiem B')
+		super(DzieckoB, self).__init__()
+		
+rodzic = Rodzic()
+dzieckoA = DzieckoA()
+dzieckoB = DzieckoB()
+
