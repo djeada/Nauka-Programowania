ff429f0b61d3e23faa333ef8dc71e642c180cd60
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2020-08-12 10:48:16 +0200

    pary

---
+++ src/Python/14.Listy2D/Zad9.py
@@ -0,0 +1,16 @@
+'''
+Otrzymujesz dwie listy 1-wymiarowe. Stworz liste 2D z par odpowiadajacych sobie elementow obu list.
+Zwroc -1 jesli listy sa roznej dlugosci.
+'''
+
+#Wersja 1
+def polaczParyV1(listaA, listaB):
+    if len(listaA) == len(listaB):
+        return -1
+
+    return [(a, b) for a, b in zip(listaA, listaB)]
+
+#Testy Poprawnosci
+listaA = [3, 2, 9]
+listaB = [1, 7, -2]
+wynik = [(3, 1), (2, 7), (9, -2)]
