d03c3b70df45ac1f082fa4770a946294698301d8
djeada <addjellouli1@gmail.com>
2020-08-18 22:40:55 +0200

    sortowanie lity obiektow

---
+++ src/Python/16.Sortowanie_Praktyka/Zad5.py
@@ -0,0 +1,21 @@
+'''
+Posortuj liste obiektow wzgledem jednego z atrybutow.
+'''
+
+class Miasto:
+	def __init__(self, nazwa, liczbaMieszkancow):
+		self.nazwa = nazwa
+		self.liczbaMieszkancow = liczbaMieszkancow
+
+	def __repr__(self):
+		return self.nazwa + ' : ' + str(self.liczbaMieszkancow)
+
+miasta = [Miasto('New York', 8400000), Miasto('Paris', 2150000), Miasto('Berlin', 3800000)]
+
+import operator
+
+for miasto in sorted(miasta, key=operator.attrgetter('nazwa')):
+    print(miasto)
+    
+for miasto in sorted(miasta, key=operator.attrgetter('liczbaMieszkancow')):
+    print(miasto)
