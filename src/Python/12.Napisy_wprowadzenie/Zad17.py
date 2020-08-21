78d3b4c453c3f3ba509d8246c06a249be2b908d3
djeada <addjellouli1@gmail.com>
2020-08-21 23:35:31 +0200

    brakujace zadanie

---
+++ src/Python/12.Napisy_wprowadzenie/Zad17.py
@@ -0,0 +1,19 @@
+'''
+Zamien liste na napis.
+'''
+
+#Wersja 1
+def zamienListeNaNapisV1(lista):
+	return ''.join(str(x) for x in lista)
+
+#Testy Poprawnosci
+lista = [2, 3, 10]
+napis = '2310'
+
+assert(zamienListeNaNapisV1(lista) == napis)
+
+lista = ['ab', 'hu', 'czo']
+napis = 'abhuczo'
+
+assert(zamienListeNaNapisV1(lista) == napis)
+
