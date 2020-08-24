486dd9c24e48e5d7520bdf889f1183f63618a59d
djeada <addjellouli1@gmail.com>
2020-08-24 23:14:16 +0200

    listy

---
+++ src/Python/24.Sortowanie_Praktyka/Zad6.py
@@ -0,0 +1,20 @@
+#Wersja 1
+#Zlozonosc czasowa O(n)
+#Zlozonosc pamieciowa O(1)
+def sortujBinarnaTabliceV1(tablica):
+
+    zera = tablica.count(0)
+
+    for i in range(zera):
+        tablica[i] = 0
+
+    for i in range(zera, len(tablica)):
+        tablica[i] = 1
+
+    return tablica
+
+#Testy Poprawnosci
+tablica = [0, 0, 1, 0, 1, 1, 0, 1, 0, 0]
+wynik = [0, 0, 0, 0, 0, 0, 1, 1, 1, 1]
+
+assert(sortujBinarnaTabliceV1(tablica) == wynik)
