5ea2273cce59c3faf0a6e6f838c06471eba8de00
djeada <addjellouli1@gmail.com>
2020-09-18 00:36:39 +0200

    tylko cyfry

---
+++ src/Python/24.Wyrazenia_regularne/Zad3.py
@@ -0,0 +1,16 @@
+'''
+Z ciagu znakow wypisz cyfry.
+'''
+
+import re
+
+#Wersja 1
+def tylkoCyfryV1(slowo):
+	return re.sub('[^\d]', '', slowo)
+
+#Testy Poprawnosci
+slowo = 'numer 32.19 wyzej niz 31.17'
+wynik = '32193117'
+
+assert(tylkoCyfryV1(slowo) == wynik)
+
