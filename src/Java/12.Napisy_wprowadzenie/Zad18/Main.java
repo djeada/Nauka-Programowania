1677cc536fed9efe13b54fc4c521991b6ad4044e
djeada <addjellouli1@gmail.com>
2020-09-19 00:39:05 +0200

    odwroc napisy w zdaniu

---
+++ src/Java/12.Napisy_wprowadzenie/Zad18/Main.java
@@ -0,0 +1,47 @@
+/*
+ Odwroc wszystkie napisy w zdaniu.
+ Znaki powinny zostac zamienione miejscami dla kazdego napisu z osobna.
+ Napisy oddzielone sa spacjami, nie zmieniaj ich kolejnosci.
+ */
+
+import java.util.stream.Collectors;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static String odwrocNapisyV1(String napis) {
+
+        String[] slowa = napis.split(" ");
+        StringBuilder odwrocony = new StringBuilder();
+
+        for (String word : slowa) {
+
+            StringBuilder reverseWord = new StringBuilder();
+
+            for (int i = word.length() - 1; i >= 0; i--) {
+                reverseWord.append(word.charAt(i));
+            }
+
+            odwrocony.append(reverseWord).append(" ");
+        }
+
+        return odwrocony.toString();
+    }
+
+    public static String odwrocNapisyV2(String napis) {
+        return Pattern.compile(" +").splitAsStream(napis)
+                .map(w -> new StringBuilder(w).reverse())
+                .collect(Collectors.joining(" "));
+    }
+
+    public static void main(String[] args) {
+    	
+    	String napis = "Cameron Diaz";
+    	String wynik = "noremaC zaiD";
+    	
+    	assert odwrocNapisyV1(napis).equals(wynik);
+    	assert odwrocNapisyV2(napis).equals(wynik);
+    	
+    }
+
+}
