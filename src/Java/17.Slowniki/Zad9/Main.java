04eb954fdfb6ee1deae08bb5717607a962fc5de3
djeada <addjellouli1@gmail.com>
2020-09-20 01:11:05 +0200

    najczesciej wystepujacy znak

---
+++ src/Java/17.Slowniki/Zad9/Main.java
@@ -0,0 +1,76 @@
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+/*
+Najczesciej wystepujacy znak.
+Ignoruj spacje.
+*/
+
+public class Main {
+	
+    public static Character najczesciejWystepujacyZnakV1(String slowo) {
+
+        Map<Character, Integer> counter = new HashMap<>();
+        
+        for (Character ch : slowo.toCharArray()) {
+
+            if (!Character.isWhitespace(ch)) {
+                Integer n = counter.get(ch);
+               
+                if (n == null) 
+                    counter.put(ch, 1);
+               
+                else
+                    counter.put(ch, ++n);
+            }
+        }
+
+        int liczbaWystapien = Collections.max(counter.values());
+
+        char znak = Character.MIN_VALUE;
+        for (Entry<Character, Integer> entry : counter.entrySet())
+            if (entry.getValue() == liczbaWystapien)
+                znak = entry.getKey();
+
+        return znak;
+    }
+
+    public static Character najczesciejWystepujacyZnakV2(String slowo) {
+
+        int liczbaWystapien = -1;
+        char znak = Character.MIN_VALUE;
+
+        int[] kodyASCII = new int[256];
+
+        for (Character ch : slowo.toCharArray()) {
+            
+        	if (!Character.isWhitespace(ch)) {
+
+                int code = (int) ch;
+                kodyASCII[code]++;
+                if (kodyASCII[code] > liczbaWystapien) {
+                    liczbaWystapien = kodyASCII[code];
+                    znak = ch;
+                }
+            }
+
+        }
+
+        return znak;
+    }
+
+    public static void main(String[] args) {
+    	
+    	String slowo = "konstantynopolitanczykowianeczka";
+    	Character wynik = 'n';
+    	    	
+    	assert najczesciejWystepujacyZnakV1(slowo).equals(wynik);
+    	assert najczesciejWystepujacyZnakV2(slowo).equals(wynik);
+    	
+
+    }
+
+}
