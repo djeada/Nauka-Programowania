6cc5b9983cde65d26de3459152e29b810a34d459
djeada <addjellouli1@gmail.com>
2020-09-20 00:54:30 +0200

    usun duplikaty

---
+++ src/Java/09.Listy_wprowadzenie/Zad9/Main.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/*
+Usun duplikaty.
+*/
+
+public class Main {
+
+	
+    public static List<Integer> usunDuplikatyV1(List<Integer> lista) {
+
+    	List<Integer> wynik = new ArrayList<Integer>();
+    	
+        for (int x : lista)
+            if (!wynik.contains(x))
+               wynik.add(x);
+
+        return wynik;
+    }
+
+    public static List<Integer> usunDuplikatyV2(List<Integer> lista) {
+
+    	List<Integer> wynik = new ArrayList<Integer>();
+        Set<Integer> zbior = new HashSet<>();
+        
+        for (int x : lista)
+            if (zbior.add(x)) 
+                wynik.add(x);
+
+        return wynik;
+    }
+
+    public static void main(String[] args) {
+    	
+    	List<Integer> lista = new ArrayList<>(Arrays.asList(2, 1, 2, 2, 3, 3));
+    	List<Integer> wynik = new ArrayList<>(Arrays.asList(2, 1, 3));
+
+    	assert usunDuplikatyV1(lista).equals(wynik);
+    	assert usunDuplikatyV2(lista).equals(wynik);
+    	
+
+    }
+
+}
