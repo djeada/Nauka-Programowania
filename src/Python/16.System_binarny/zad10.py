40317a8bc9cea79d40669375dcdad50fbbc54848
djeada <addjellouli1@gmail.com>
2020-09-21 22:56:43 +0200

    dodano nowe zadania: listy oraz system binarny

---
+++ src/Python/16.System_binarny/zad10.py
@@ -0,0 +1,23 @@
+'''
+Ile bitow nalezy odwrocic by zmienic A w B?
+'''
+
+def bityDoZmianyV1(liczbaA, liczbaB):
+
+	n = liczbaA ^ liczbaB
+
+	licznikJedynek = 0
+	while n:
+		n &= (n - 1) 
+		licznikJedynek += 1
+
+	return licznikJedynek
+
+if __name__ == '__main__':
+
+	liczbaA = 34
+	liczbaB = 73
+	wynik = 5
+	
+	assert(bityDoZmianyV1(liczbaA, liczbaB) == wynik)
+	
---
+++ src/Python/24.Listy_trudne/Zad5.py
@@ -0,0 +1,56 @@
+'''
+Polacz m posortowanych list.
+Elementy sa unikalne.
+'''
+
+import heapq
+
+class Node(object):
+	def __init__(self, val: int, i: int, index: int):
+		self.val = val
+		self.i = i
+		self.index = index
+
+	def __lt__(self, other):
+		return self.val < other.val
+
+#Zlozonosc czasowa: O(nlogm)
+#Zalozenie pop/push O(logm)
+def polaczListyV1(lista):
+
+	M = len(lista)
+
+	heap = [Node(lista[i][0], i, 0) for i in range(M) if len(lista[i]) >= 1]
+	heapq.heapify(heap)
+	wynik = []
+
+	while len(heap) > 0:
+
+		min = heapq.heappop(heap)
+		wynik.append(min.val)
+		
+		if min.index + 1 < len(lista[min.i]):
+			min.index += 1;
+			min.val = lista[min.i][min.index];
+			heapq.heappush(heap, min);
+	
+	return wynik
+	
+#Zlozonosc czasowa: O(n^2)
+def polaczListyV2(lista):
+	return sorted([x for y in lista for x in y])
+
+if __name__ == '__main__':
+
+	lista = [[ -6, 23, 29, 33],
+		[ 6, 22, 35, 71 ],
+		[ 5, 19, 21, 37 ],
+		[ -12, -7, -3, 28 ]]
+
+	wynik = [-12, -7, -6, -3, 5, 6, 19, 21, 22, 23, 28, 29, 33, 35, 37, 71]
+
+	print(polaczListyV1(lista))
+
+	assert(polaczListyV1(lista) == wynik)
+	assert(polaczListyV2(lista) == wynik)
+
