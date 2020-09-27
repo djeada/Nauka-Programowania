7c2970b3d786fcec959db94560b117f050d1e7b9
djeada <addjellouli1@gmail.com>
2020-09-28 00:12:04 +0200

    poprawki odnosnie formatowania

---
+++ src/Cpp/01.Interakcja_z_konsola/Zad1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	/*
+	Wypisz na konsole swoje inicjaly.
+	*/
+	
+	cout << "A.D." <<endl;
+	
+	/*
+	Wypisz na konsole "witaj swiecie"
+	*/
+
+	cout << "Witaj swiecie" <<endl;
+		
+	return 0;
+}
+
---
+++ src/Cpp/01.Interakcja_z_konsola/Zad2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	/*
+	Wypisz na konsole najpierw druga potem pierwsza pobrana dana.
+	*/
+
+	int a, b;
+	
+	cin >> a >> b;
+	
+	cout << b << endl << a;
+	
+	return 0;
+}
