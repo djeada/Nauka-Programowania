ecc0a7fa59921c8be90e0b3dd00d785c882095d1
Mumbak <zinedin1000@gmail.com>
2020-10-17 17:53:33 +0200

    Dodano zadanie 3 i 4

---
+++ src/Python/02.Instrukcje_sterujace/Zad3.py
@@ -0,0 +1,13 @@
+'''
+Dla pobranej liczby, sprawdz czy liczba jest ujemna, dodatnia, czy jest zerem.
+'''
+print("podaj liczbe")
+
+a = int(input())
+
+if a < 0:
+	print("libczba jest ujemna")
+elif a > 0:
+	print("liczba jest dodatnia")
+else:
+	print("liczba jest zerem")
