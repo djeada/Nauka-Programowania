06084bfee48690d7d2c54eaecc75072211f55bc4
Mumbak <zinedin1000@gmail.com>
2020-10-17 16:36:59 +0200

    Zaktualizowano zadanie 7

--- src/Python/01.Interakcja_z_konsola/Zad7.py
+++ src/Python/01.Interakcja_z_konsola/Zad7.py
@@ -9,4 +9,14 @@ h = int(input())
 
 print("pole trojkata o podstawie", a , "i wyskosci", h , "jest rowne", a*h/2)
 
+''' 
+Pobierz dlogosci bokow prostokata i wypisz pole.
+''' 
+
+print("podaj dlogosci bokow prostokata i wypisz pole")
+
+a= int(input())
+b= int(input())
+
+print("pole prostokata o boku" , a , "i boku b" , b , "jest rowne", a*b) 
 
