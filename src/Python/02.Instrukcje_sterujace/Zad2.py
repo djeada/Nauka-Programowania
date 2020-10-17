31b8a3cf310380b6b4934bd8c584af0b288175d6
Mumbak <zinedin1000@gmail.com>
2020-10-17 17:22:10 +0200

    Dodano zadanie 2 porownanie liczb

---
+++ src/Python/02.Instrukcje_sterujace/Zad2.py
@@ -0,0 +1,12 @@
+'''
+Dla dwoch pobranych liczb, sprawdz czy pobrane liczby sa identyczne.
+'''
+print("podaj dwie liczby")
+
+a = int(input())
+b = int(input())
+
+if a == b :
+	print("liczby sa jednakowe")
+else: 
+	print("liczby sa rozne")
