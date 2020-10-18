f62d908de6e6d1e5a6ad8bb8d5a4b2f0a89e18d7
djeada <addjellouli1@gmail.com>
2020-10-18 21:04:22 +0200

    nowe zadania bash

---
+++ src/Bash/01.Interakcja_z_konsola/Zad7.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+main() {
+	    
+	#Pobierz podstawe i wysokosc trojkata i wypisz pole. 
+
+    echo "Podaj podstawe i wysokosc trojkata: "
+    read a
+    read h
+
+    echo "Pole trojkata o podstawie $a i wysokosci $h jest rowne $(($a*$h/2))"
+    
+    #Pobierz dlogosci bokow prostokata i wypisz pole. 
+
+    echo "Podaj dlugosci bokow prostokata: "
+    read a
+    read b
+
+    echo "Pole prostokata o bokach $a i $b jest rowne $(($a*$b))"	
+    	       
+}
+
+main "$@"
+
