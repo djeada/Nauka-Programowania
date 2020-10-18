4f1522ac679b611c0f67fce6f0e76c44b6c584eb
djeada <addjellouli1@gmail.com>
2020-10-19 00:48:12 +0200

    dodano zadanie: slowa elfickie

---
+++ src/Cpp/15.Funkcje_rekurencja/Zad8.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cassert>
+#include <algorithm>
+
+/*
+Napisz funkcje, ktora sprawdzi czy otrzymane slowo jest elfickie. 
+Elfickie slowo to takie, w ktorym co najmniej raz wystepuje kazda 
+z liter slowa elf.
+*/
+
+using namespace std;
+
+bool czyElfickieSlowoV1(string slowo, string elf = "elf", int i = 0) {
+
+	if (i >= slowo.size())
+		return false;
+		
+	auto litera = slowo[i];
+
+	if (elf.find(litera) != string::npos) {
+			elf.erase(remove(elf.begin(), elf.end(), litera), elf.end());
+	 		if (elf.empty())
+	 			return true;
+	}
+	
+	return czyElfickieSlowoV1(slowo, elf, i + 1);
+}
+
+int main () {
+
+	string slowo = "reflektor";
+	bool wynik = true;
+	
+	assert(czyElfickieSlowoV1(slowo) == wynik);
+	
+	slowo = "elzbieta";
+	wynik = false;
+	
+	assert(czyElfickieSlowoV1(slowo) == wynik);
+	
+	return 0;
+}
+
+
