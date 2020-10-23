8d7db3a47893479ee30da85a57d3f89af4993b41
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2020-10-23 09:51:30 +0200

    Bash if

---
+++ src/Bash/02.Instrukcje_sterujace/Zad6.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+#Znajdz maksimum z czterech liczb.
+
+main() {
+    
+    echo "Podaj cztery liczby."
+    read a
+    read b
+	read c
+	read d
+
+    maxAB=$([ $a -gt $b ] && echo "$a" || echo "$b")
+    maxCD=$([ $c -gt $d ] && echo "$c" || echo "$d")
+    max=$([ $maxAB -gt $maxCD ] && echo "$maxAB" || echo "$maxCD")
+
+	echo $max
+}
+
+main "$@"
+
---
+++ src/Bash/02.Instrukcje_sterujace/Zad8.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+#Pobierz od uzytkownika trzy dlugosci bokow i sprawdz czy mozna z nich zbudowac trojkat.
+
+main() {
+
+    echo "Podaj trzy dlugosci bokow: "
+
+	read a
+	read b
+	read c
+
+	if [[ $(($a + $b)) > $c ]] && [[ $(($a + $c)) > $b ]] && [[ $(($b + $c)) > $a ]]; then
+		echo "Z podanych dlugosci bokow mozna zbudowac trojkat"
+
+	else
+		echo "Z podanych dlugosci bokow nie mozna zbudowac trojkata"
+	fi
+}
+
+main "$@"
+
