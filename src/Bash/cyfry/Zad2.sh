bba9cfa0b66bbaea6694248b07e9e952f7e31a6d
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2020-11-02 12:43:56 +0100

    Dodano zadania: petle cyfry (Bash)

---
+++ src/Bash/cyfry/Zad2.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#Dla pobranej od uzytkownika liczby, zwroc liczbe cyfr pobranje liczby.
+
+main() {
+	
+	echo "Podaj liczbe: "    
+	read a
+
+
+	while [ $a -gt 0 ] 
+	do
+		echo $(($a % 10))
+		a=$(( $a / 10 )) 
+	done
+
+}
+
+main "$@"
+
---
+++ src/Bash/cyfry/Zad4.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+#Dla pobranej liczby, powinny zostac zwrocone cyfry liczby spelniajce nastepujacy warunek:
+
+main() {
+	
+	echo "Podaj liczbe: "    
+	read a
+
+	#a) Liczby parzyste;
+
+	pom=$a
+	echo "Cyfry liczby badace liczbami parzystymi: "
+
+	while [ $pom -gt 0 ] 
+	do
+		cyfra=$(($pom % 10))
+		
+		if [[ $(($cyfra % 2)) -eq 0 ]]; then
+			echo $cyfra
+		fi
+		
+		pom=$(( $pom / 10 )) 
+	done
+
+	#b) Mniejsze niz 5;
+
+	pom=$a
+	echo "Cyfry liczby mniejsze niz 5: "
+
+	while [ $pom -gt 0 ] 
+	do
+		cyfra=$(($pom % 10))
+		
+		if [[ $cyfra -lt 5 ]]; then
+			echo $cyfra
+		fi
+		
+		pom=$(( $pom / 10 )) 
+	done
+
+	#c) Rozne od 0;
+
+	pom=$a
+	echo "Cyfry liczby rozne od 0: "
+
+	while [ $pom -gt 0 ] 
+	do
+		cyfra=$(($pom % 10))
+		
+		if [[ $cyfra -ne 0 ]]; then
+			echo $cyfra
+		fi
+		
+		pom=$(( $pom / 10 )) 
+	done
+
+}
+
+main "$@"
+
