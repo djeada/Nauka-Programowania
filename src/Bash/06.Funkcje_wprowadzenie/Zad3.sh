b004b42cace2c4c2da19590ccd1ebb9ffcdfcece
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2020-11-04 11:19:42 +0100

    dodano zadania: funkcje bash

---
+++ src/Bash/06.Funkcje_wprowadzenie/Zad3.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+#Pobierz dwie liczby od uzytkownika. Napisz funkcje, ktora 
+#dla otrzymanych dwoch liczb ustali czy:
+
+#a) pierwsza liczba jest wieksza od drugiej
+
+czyWieksza() {
+
+	if [[ $1 -gt $2 ]] ; then
+        echo true
+    else
+        echo false
+    fi
+}
+
+#b) czy suma liczb jest mneijsza niz 10
+
+czySumaMniejszaOd10() {
+	suma=$(($1 + $2))
+
+	if [[ $suma -lt 10 ]] ; then
+        echo true
+    else
+        echo false
+    fi
+}
+
+#c) czy obie sa nieparzyste
+
+czyObieNieparzyste() {
+	if [[ $(($1 % 2)) -eq 1 ]] && [[ $(($2 % 2)) -eq 1 ]]; then
+        echo true
+    else
+        echo false
+    fi
+}
+
+#d) czy wieksza liczba jest mniejsza od pierwszej podniesionej do kwadratu
+
+wieksza() {
+	if [[ $1 > $2 ]]; then
+        return $1
+    else
+        return $2
+    fi
+}
+
+czyWiekszaJestMniejszaOdPierwszejKwad() {
+	wieksza $1 $2
+  	wieksza=$?
+
+	if [[ $wieksza -lt $(($1**2)) ]]; then
+        echo true
+    else
+        echo false
+    fi
+}
+
+main() {
+    echo "Podaj dwie liczby: "
+	read a
+	read b
+
+	wynik=$(czyWieksza $a $b)
+    echo "Pierwsza liczba jest wieksza od drugiej: $wynik"
+
+	wynik=$(czySumaMniejszaOd10 $a $b)
+    echo "Suma liczb jest mniejsza od 10: $wynik"
+
+	wynik=$(czyObieNieparzyste $a $b)
+    echo "Obie liczby nieparzyste: $wynik"
+
+	wynik=$(czyWiekszaJestMniejszaOdPierwszejKwad $a $b)
+    echo "Wieksza liczba jest mniejsza od pierwszej podniesionej do kwadratu $wynik"
+}
+
+main "$@"
+
---
+++ src/Bash/06.Funkcje_wprowadzenie/Zad4.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+
+#Napisz funkcje, ktora:
+
+#a) dla podanych dwoch liczb, zwroci mniejsza
+
+min2() {
+
+	if [[ $1 -lt $2 ]] ; then
+		return $1
+    else
+        return $2
+    fi
+}
+
+#b) dla podanych dwoch liczb, zwroci wieksza
+
+maks2() {
+
+	if [[ $1 -gt $2 ]] ; then
+		return $1
+    else
+        return $2
+    fi
+}
+
+
+#c) dla podanych trzech liczb, zwroci najmniejsza
+
+min3() {
+	if [[ $1 -le $2 && $1 -le $3 ]]; then
+		    return $1
+	elif [[ $2 -le $1 && $2 -le $3 ]]; then
+		    return $2
+	else
+		    return $3
+	fi
+}
+
+#d) dla podanych trzech liczb, zwroci najwieksza
+
+maks3() {
+	if [[ $1 -ge $2 && $1 -ge $3 ]]; then
+		    return $1
+	elif [[ $2 -ge $1 && $2 -ge $3 ]]; then
+		    return $2
+	else
+		    return $3
+	fi
+}
+
+main() {
+    echo "Podaj dwie liczby: "
+	read a
+	read b
+
+	min2 $a $b
+	wynik=$?
+    echo "mniejsza liczba to $wynik"
+
+	maks2 $a $b
+	wynik=$?
+    echo "wieksza liczba to $wynik"
+
+	echo "Podaj trzy liczby: "
+	read a
+	read b
+	read c
+
+	min3 $a $b $c
+	wynik=$?
+    echo "najmniejsza liczba to $wynik"
+
+	maks3 $a $b $c
+	wynik=$?
+    echo "najwieksza liczba to $wynik"
+
+}
+
+main "$@"
+
