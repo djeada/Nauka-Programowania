f5fdee60dce34aa77fdd2ec5fda6968d1644dfc4
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2020-11-10 12:05:26 +0100

    Dodano zadania: petla algorytmy matematyczne

---
+++ src/Bash/07.Petla_algorytmy_matematyczne/Zad1.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+#Napisz funkcje, ktora dla otrzymanej liczby n, poprosi 
+#uzytkownika o podanie n liczb i zwroci ich srednia aryt.
+
+srednia() {
+	
+	n=$1
+	suma=0
+	
+	for (( i=0; i<$n; i++ )); do 
+	  read x             
+	  suma=$(($suma + $x))   
+	done
+
+	wynik=$(bc -l <<< "scale=2; $suma/$n")
+
+	echo $wynik
+}
+
+main() {
+    echo "Podaj liczbe n: "
+	read n
+
+	echo "Wprowadz $n liczb: "
+	wynik=$(srednia $n)
+    echo "srednia arytmetyczna z podanych liczb: $wynik"
+}
+
+main "$@"
+
