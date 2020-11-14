72cf7001322de8ccbc10cb01942686dd75410137
Mumbak <zinedin1000@gmail.com>
2020-11-14 17:17:22 +0100

    Zad 3

---
+++ src/Python/03. Daty/Zad2.py
@@ -0,0 +1,19 @@
+
+'''
+
+Dla pobranych szesciu liczb, dnia urodzenia osoby, miesiaca urodzenia osoby, roku
+urodzenia osoby oraz aktualnego dnia, aktualnego miesiaca, aktualnego roku sprawdz czy
+dana osoba jest pelnoletnia.
+
+''' 
+
+print("podaj dzien, miesiac, rok urodzenia i aktualny dzien, miesiac oraz rok") 
+
+dzien_u =    int(input())
+miesiac_u =  int(input())
+rok_u =      int(input())
+dzien_a =    int(input())
+miesiac_a =  int(input())
+rok_a =      int(input())
+
+if 	
