18717534e37100be3250b061c4d3ded95f872cc8
djeada <addjellouli1@gmail.com>
2020-11-17 23:05:47 +0100

    dodano zadanie 9 - listy 2d cpp

---
+++ src/Cpp/13.Listy_2D/Zad9.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cassert>
+#include <vector>
+
+using namespace std;
+
+/*
+Otrzymujesz liste 2d. Obroc otrzymana liste o 90 stopni. 
+Tzn. zamien miejscami ostatnia kolumne, z ostatnim wierszem, 
+przedostatnią kolumnę z przedostatnim wierszem itd.
+*/
+
+void obroc(vector<vector<int>>& macierz) {
+
+	int N = macierz.size();
+
+	for(int i = 0; i < N/2; i++) {
+		int first = i + 1;
+		int last = N - 1 - i;
+		
+		for(int j = first; j < last; j++) {
+			int pom = macierz[i][j];		
+			macierz[i][j] = macierz[N-1-j][i];
+			macierz[N-1-j][i] = macierz[N-1-i][N-1-j];
+			macierz[N-1-i][N-1-j] = macierz[j][N-1-i];
+			macierz[j][N-1-i] = pom;
+		}
+
+		int pom = macierz[i][i];
+		macierz[i][i] = macierz[N-1-i][i];
+		macierz[N-1-i][i] = macierz[N-1-i][N-1-i];
+		macierz[N-1-i][N-1-i] = macierz[i][N-1-i];
+		macierz[i][N-1-i] = pom;
+	}
+}
+
+int main() {
+
+	vector<vector<int>> macierz { { 1, 2, 3 },
+                                { 4, 5, 6 },
+								{ 7, 8, 9} };
+	
+	vector<vector<int>> wynik { { 7, 4, 1 },
+                                { 8, 5, 2 },
+								{ 9, 6, 3} };
+
+	obroc(macierz);
+
+	assert(macierz == wynik);
+
+	return 0;	 	
+}
