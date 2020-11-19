e5054990452171b8f4aaae0ff29047b217c6a614
djeada <addjellouli1@gmail.com>
2020-11-19 18:32:25 +0100

    najdluzsze powtorzenie - cpp

---
+++ src/Cpp/25.Napisy_trudne/Zad7.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cassert>
+
+using namespace std;
+
+/*
+Znajdz najdluzsze powtorzenie w napisie.
+*/
+
+string nwp(string& slowoA, string& slowoB) {
+
+	int N = slowoA.length() < slowoB.length() ? slowoA.length() : slowoB.length();  
+	
+	for (int i = 0; i < N; i++) {
+		if (slowoA[i] != slowoB[i])
+			return slowoA.substr(0, i);  
+	}
+	
+	return slowoA.substr(0, N);
+}
+
+   
+
+string najdluzszePowtorzenieV1(string& slowo) {
+
+	string maks;
+
+	int N = slowo.length();
+
+	for (int i = 0; i < N; i++) {
+		for (int j = i + 1; j < N; j++) {
+			string slowoA = slowo.substr(i, N);
+			string slowoB = slowo.substr(j, N);
+			string podslowo = nwp(slowoA, slowoB);
+
+			if (podslowo.length() > maks.length())
+				maks = podslowo;
+		}
+	}
+
+	return maks;
+}
+
+int main() {
+
+	//Testy Poprawnosci
+    string slowo = "98432934021742343230";
+	string wynik = "432";
+
+	assert(najdluzszePowtorzenieV1(slowo) == wynik);
+
+    return 0;
+}
+
