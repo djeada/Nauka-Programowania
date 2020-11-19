b847f9608b632074582eceb84d75a32f4daf2b80
djeada <addjellouli1@gmail.com>
2020-11-19 14:24:06 +0100

    zmiana organizacji zadan

--- src/Python/24.Listy_trudne/Zad4.py
+++ src/Python/24.Listy_trudne/Zad4.py
@@ -1,25 +1,44 @@
 '''
-Znajdz zbior potegowy danego zbioru.
+Otrzymujesz dwie listy binarne, A oraz B. 
+Znajdz dlugosc takich podciagow obu list o rownej sumie, ktorych suma jest najwieksza.
+Podciagi nie musza byc takie same.
 '''
 
-#Zlozonosc O(n.2^n) 
-def zbiorPotegowy(lista):
-	#wymaga sortowania O(nlogn)
-	lista.sort()
+#Wersja 1
+#Zlozonosc czasowa O(n)
+#Zlozonosc pamieciowa O(n)
+def znajdzPodciagV1(listaA, listaB):
 	
-	N = int(pow(2, len(lista)))
-	zbiorPotegowy = set()
-	
-	for i in range(N):
-		subset = [lista[j] for j in range(len(lista)) if i & (1 << j)]
-		zbiorPotegowy.add(tuple(subset))
+	if len(listaA) != len(listaB):
+		return False
 
-	return zbiorPotegowy
+	histo = {}
+
+	histo[0] = -1
+
+	wynik, sumaA, sumaB = 0, 0, 0
+
+	for i in range(len(listaA)):
+
+		sumaA += listaA[i]
+		sumaB += listaB[i]
+
+		roznica = sumaA - sumaB
+
+		if roznica not in histo:
+			histo[roznica] = i
+
+		else:
+			wynik = max(wynik, i - histo[roznica])
+
+	return wynik
 
 if __name__ == '__main__':
-
-	lista = [1, 2, 1]
-	wynik = {(1, 2), (1,), (2,), (1, 1, 2), (), (1, 1)}
 	
-	assert(sorted(zbiorPotegowy(lista)) == sorted(wynik))
+	#Testy poprawnosci
+	listaA = [0, 0, 1, 1, 1, 1]
+	listaB = [0, 1, 1, 0, 1, 0]
+	wynik = 5
+
+	print(znajdzPodciagV1(listaA, listaB))
 
--- src/Python/24.Listy_trudne/Zad5.py
+++ src/Python/24.Listy_trudne/Zad5.py
@@ -1,56 +1,25 @@
 '''
-Polacz m posortowanych list.
-Elementy sa unikalne.
+Znajdz zbior potegowy danego zbioru.
 '''
 
-import heapq
-
-class Node(object):
-	def __init__(self, val: int, i: int, index: int):
-		self.val = val
-		self.i = i
-		self.index = index
-
-	def __lt__(self, other):
-		return self.val < other.val
-
-#Zlozonosc czasowa: O(nlogm)
-#Zalozenie pop/push O(logm)
-def polaczListyV1(lista):
-
-	M = len(lista)
-
-	heap = [Node(lista[i][0], i, 0) for i in range(M) if len(lista[i]) >= 1]
-	heapq.heapify(heap)
-	wynik = []
-
-	while len(heap) > 0:
-
-		min = heapq.heappop(heap)
-		wynik.append(min.val)
-		
-		if min.index + 1 < len(lista[min.i]):
-			min.index += 1;
-			min.val = lista[min.i][min.index];
-			heapq.heappush(heap, min);
+#Zlozonosc O(n.2^n) 
+def zbiorPotegowy(lista):
+	#wymaga sortowania O(nlogn)
+	lista.sort()
 	
-	return wynik
+	N = int(pow(2, len(lista)))
+	zbiorPotegowy = set()
 	
-#Zlozonosc czasowa: O(n^2)
-def polaczListyV2(lista):
-	return sorted([x for y in lista for x in y])
+	for i in range(N):
+		subset = [lista[j] for j in range(len(lista)) if i & (1 << j)]
+		zbiorPotegowy.add(tuple(subset))
+
+	return zbiorPotegowy
 
 if __name__ == '__main__':
 
-	lista = [[ -6, 23, 29, 33],
-		[ 6, 22, 35, 71 ],
-		[ 5, 19, 21, 37 ],
-		[ -12, -7, -3, 28 ]]
-
-	wynik = [-12, -7, -6, -3, 5, 6, 19, 21, 22, 23, 28, 29, 33, 35, 37, 71]
-
-	print(polaczListyV1(lista))
-
-	assert(polaczListyV1(lista) == wynik)
-	assert(polaczListyV2(lista) == wynik)
+	lista = [1, 2, 1]
+	wynik = {(1, 2), (1,), (2,), (1, 1, 2), (), (1, 1)}
+	
+	assert(sorted(zbiorPotegowy(lista)) == sorted(wynik))
 
