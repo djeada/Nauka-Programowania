c3693abbc9105579dfdca1c28f5ae0d4c86c2f50
djeada <addjellouli1@gmail.com>
2020-11-19 18:48:23 +0100

    przesun zera - cpp

---
+++ src/Cpp/24.Listy_trudne/Zad2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cassert>
+#include <vector>
+
+using namespace std;
+
+/*
+Przesun zera na koniec listy.
+*/
+
+void swap(vector<int>& lista, int i, int j) {
+	int temp = lista[j];
+	lista[j] = lista[i];
+	lista[i] = temp;
+}
+
+
+void zeraV1(vector<int>& lista) {
+	
+	int N = lista.size();
+
+	for (int i = 0; i < N; i++) {
+		if (lista[i] == 0) {
+			int j = i + 1;
+			
+			while (lista[j] == 0 && j < N)
+				j++;
+			
+			swap(lista, i, j);
+		}
+	}
+}
+
+int main() {
+
+	//Testy Poprawnosci
+    vector<int> lista {0, 1, 3, 0, 8, 12, 0, 4, 0, 7, 0};
+    vector<int> wynik {1, 3, 8, 12, 4, 7, 0, 0, 0, 0, 0};
+	
+	zeraV1(lista);
+	assert(lista == wynik);
+
+    return 0;
+}
+
