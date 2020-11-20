340dffea6d653db5f15f436ed2b8d77fe1fca692
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2020-11-20 15:54:00 +0100

    poprawiono zad 6 - wprowadzenie Python

--- src/Python/01.Interakcja_z_konsola/Zad6.py
+++ src/Python/01.Interakcja_z_konsola/Zad6.py
@@ -4,67 +4,67 @@ if __name__ == '__main__':
 	Pobierz wielkosc w kilogramach i wypisz ilu gramom odpowiada.
 	'''
 
-	print("podaj kg")
+	print("podaj wielkosc w kilogramach:")
 
 	kilogramy = int(input())
 	gramy = kilogramy * 1000
 
-	print(kilogramy, "kg to " , gramy, "g") 
+	print(kilogramy, " kg to " , gramy, " g") 
 
 	'''
 	Pobierz wielkosc w calach i wypisz ilu centymetrom odpowiada.
 	'''
 
-	print("podaj cale")
+	print("podaj wielkosc w calach:")
 
 	cal = int(input())
 	cm = cal / 2.54
 
-	print(cal, "cali to" , cm, "cm") 
+	print(cal, " cali to" , cm, " cm") 
 	 
 	'''
 	Pobierz ilosc sekund i przelicz na godziny.
 	'''
 
-	print("podaj ilosc sekund")
+	print("podaj ilosc sekund:")
 
-	s = int(input())
-	h = s / 3600
+	sekundy = int(input())
+	godziny = sekundy / 3600
 
-	print(s, "sekund to " , h, "godzin")
+	print(sekundy, " sekund to " , godziny, " godzin")
 
 	'''
 	Pobierz liczbe w euro i wypisz ilu zlotowkom odpowiada. 
 	'''
 
-	print("podaj liczbe w euro")
+	print("podaj liczbe w euro:")
 
 	euro = input()
 	zloty = euro * 4.40
 
-	print(euro, "euro to " , zloty, "zlotych")
+	print(euro, " euro to " , zloty, " zlotych")
 
 	'''
 	Pobierz miare kata w stopniach i wypisz ilu radianom odpowiada. 
 	'''
 	
-	print("podaj miare kata w stopniach") 
+	print("podaj miare kata w stopniach:") 
 
-	kat = int(input())
-	rad = kat * 0.0174532
+	katStopnie = int(input())
+	katRad = katStopnie * 0.0174532
 
-	print(kat, " stopni to " , rad, "radianow")
+	print(katStopnie, " stopni to " , katRad, " radianow")
 
 	'''
 	Pobierz temperature w stopniach Farenheita i wypisz ilu stopniom Celsjusza oraz ilu stopniom Kelwina odpowiada. 
 	'''
 
-	print("podaj temperature w Farenheitach") 
+	print("podaj temperature w Farenheitach:") 
 
-	F = int(input())
-	C = (F -32) * 5 / 9
-	K = C - 273
+	tempF = int(input())
+	tempC = (tempF -32) * 5 / 9
+	tempK = tempC - 273
 
-	print(F, "Farenheitow to " , C, "C" , " i " ,K ,"K")
+	print(tempF, " F to ", tempC, " C" , " i " , tempK, " K")
 
 
