0b959f73ce8c1faada7467196a65b37da546ee9d
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2020-11-20 16:02:05 +0100

    poprawiono literowke - zad 7 wprowadzenie Python

--- src/Python/01.Interakcja_z_konsola/Zad7.py
+++ src/Python/01.Interakcja_z_konsola/Zad7.py
@@ -5,21 +5,21 @@ if __name__ == '__main__':
 	Pobierz podstawe i wysokosc trojkata i wypisz pole. 
 	''' 
 
-	print("podaj podstawe i wysokosc trojkata")
+	print("podaj podstawe i wysokosc trojkata:")
 
 	a = int(input())
 	h = int(input())
 
-	print("pole trojkata o podstawie ", a , " i wyskosci ", h , " jest rowne", a*h/2)
+	print("pole trojkata o podstawie ", a , " i wysokosci ", h , " jest rowne ", a*h/2)
 
 	''' 
-	Pobierz dlogosci bokow prostokata i wypisz pole.
+	Pobierz dlugosci bokow prostokata i wypisz pole.
 	''' 
 
-	print("podaj dlogosci bokow prostokata")
+	print("podaj dlogosci bokow prostokata:")
 
-	a= int(input())
-	b= int(input())
+	a = int(input())
+	b = int(input())
 
 	print("pole prostokata o bokach " , a , " i " , b , " jest rowne ", a*b) 
 
