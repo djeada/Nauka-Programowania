10ee5e644d20a2bfa143a81bffa54728ccef6050
djeada <addjellouli1@gmail.com>
2020-11-23 23:40:32 +0100

    dodano zadania interakcja z konsola - rust

---
+++ src/Rust/01.Interakcja_z_konsola/Zad3.rs
@@ -0,0 +1,38 @@
+
+fn main() {
+	//Narysuj ksztalt.
+
+	/*
+	Kwadrat.
+	XX
+	XX
+	*/
+
+	println!("xx");
+	println!("xx");
+
+	/*Trojkat.
+	1
+	22
+	333
+	*/
+	
+	println!("1");
+	println!("22");
+	println!("333");
+	
+	/*Romb z jedynek.
+	  1
+	 111
+	11111
+	 111
+ 	 1
+ 	*/
+  	
+  	println!("  1");
+  	println!(" 111");
+  	println!("11111");
+  	println!(" 111");
+  	println!("  1");			
+}
+
