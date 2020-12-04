d3681cd12dafb0e35d9f812f97e354a911dbf976
djeada <addjellouli1@gmail.com>
2020-12-04 22:27:13 +0100

    dodano zadanie potega - Bash

---
+++ src/Bash/07.Petla_algorytmy_matematyczne/Zad2.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+source ../assert.sh
+
+#Napisz funkcje, ktora dla otrzymanych dwoch liczb 
+#przy pomocy petli obliczy ile wynosi a podniesione do b.
+
+potega() {
+	
+	a=$1
+	b=$2
+	wynik=1
+	
+	for (( i=0; i<$b; i++ )); do 
+		wynik=$(($wynik * $a))
+	done
+
+	echo $wynik
+}
+
+main() {
+	a=2
+	b=3
+	wynik=8
+	assert "$wynik -eq $(potega $a $b)" $LINENO
+}
+
+main "$@"
+
