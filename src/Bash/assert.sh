7d7b0944fd70256d8351211f077241b970ff4801
djeada <addjellouli1@gmail.com>
2020-12-05 19:15:04 +0100

    poszerzono mozliwosci testow o funkcje assertTrue i assertFalse

--- src/Bash/assert.sh
+++ src/Bash/assert.sh
@@ -13,4 +13,30 @@ assert () {
 		echo "File \"$0\", line $lineno"
 		exit $E_ASSERT_FAILED
 	fi
-}   
+}  
+
+assertTrue () {
+	E_ASSERT_FAILED=99
+	E_PARAM_ERR=98
+	lineno=$2
+
+	if [ "$1" = false ]; then
+		echo "Assertion failed:  \"$1\""
+		echo "File \"$0\", line $lineno"
+		exit $E_ASSERT_FAILED
+	fi
+
+}
+
+assertFalse () {
+	E_ASSERT_FAILED=99
+	E_PARAM_ERR=98
+	lineno=$2
+
+	if [ "$1" = true ]; then
+		echo "Assertion failed:  \"$1\""
+		echo "File \"$0\", line $lineno"
+		exit $E_ASSERT_FAILED
+	fi
+
+}
