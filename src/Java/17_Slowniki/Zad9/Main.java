b737ce85e10dfef38dc6d2d90f945642e87b6b87
djeada <addjellouli1@gmail.com>
2020-12-22 22:09:44 +0100

    zmiana organizacji zadan

--- src/Java/17_Slowniki/Zad9/Main.java
+++ src/Java/17_Slowniki/Zad9/Main.java
@@ -1,76 +1,55 @@
-
-import java.util.Collections;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Map;
-import java.util.Map.Entry;
-
-/*
-Najczesciej wystepujacy znak.
-Ignoruj spacje.
-*/
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
-	
-    public static Character najczesciejWystepujacyZnakV1(String slowo) {
 
-        Map<Character, Integer> counter = new HashMap<>();
-        
-        for (Character ch : slowo.toCharArray()) {
+    public static List<Character> policzPowtarzajaceSieZnakiV1(String slowo) {
+    	
+    	List<Character> listaZnakow = new ArrayList<Character>();
+    	
+        if (slowo != null) {
+        	  Map<Character, Integer> histo = new HashMap<>();                
 
-            if (!Character.isWhitespace(ch)) {
-                Integer n = counter.get(ch);
-               
-                if (n == null) 
-                    counter.put(ch, 1);
-               
-                else
-                    counter.put(ch, ++n);
-            }
-        }
-
-        int liczbaWystapien = Collections.max(counter.values());
-
-        char znak = Character.MIN_VALUE;
-        for (Entry<Character, Integer> entry : counter.entrySet())
-            if (entry.getValue() == liczbaWystapien)
-                znak = entry.getKey();
-
-        return znak;
+        	  for (char ch : slowo.toCharArray()) {
+        		  
+        		  histo.compute(ch, (k, v) -> (v == null) ? 1 : ++v);
+        		  
+        		  if (histo.get(ch) > 1 && !listaZnakow.contains(ch))
+        			  listaZnakow.add(ch);
+        	  }   
+        }  
+           
+        return listaZnakow;
     }
 
-    public static Character najczesciejWystepujacyZnakV2(String slowo) {
 
-        int liczbaWystapien = -1;
-        char znak = Character.MIN_VALUE;
+    public static List<Character> policzPowtarzajaceSieZnakiV2(String slowo) {
+    	List<Character> listaZnakow = new ArrayList<Character>();
 
-        int[] kodyASCII = new int[256];
-
-        for (Character ch : slowo.toCharArray()) {
-            
-        	if (!Character.isWhitespace(ch)) {
-
-                int code = (int) ch;
-                kodyASCII[code]++;
-                if (kodyASCII[code] > liczbaWystapien) {
-                    liczbaWystapien = kodyASCII[code];
-                    znak = ch;
-                }
-            }
-
+        if (slowo != null) {
+        	 Map<Character, Long> histo = slowo.chars()
+                     .mapToObj(c -> (char) c)
+                     .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+        
+	        for (Map.Entry<Character, Long> para : histo.entrySet())
+	        	 if (para.getValue() > 1 && !listaZnakow.contains(para.getKey()))
+	   			  	listaZnakow.add(para.getKey());
         }
-
-        return znak;
+        
+        return listaZnakow;
     }
 
     public static void main(String[] args) {
-    	
-    	String slowo = "konstantynopolitanczykowianeczka";
-    	Character wynik = 'n';
-    	    	
-    	assert najczesciejWystepujacyZnakV1(slowo).equals(wynik);
-    	assert najczesciejWystepujacyZnakV2(slowo).equals(wynik);
-    	
+        
+        String zdanie = "Halo nazywam sie Pan";
+    	List<Character> listaZnakow = Arrays.asList('a', ' ', 'n');
+
+    	assert policzPowtarzajaceSieZnakiV1(zdanie).equals(listaZnakow);
+    	assert policzPowtarzajaceSieZnakiV2(zdanie).equals(listaZnakow);
 
     }
-
 }
