988c3013057c93430733714c9d1dc9a8b7adb470
djeada <addjellouli1@gmail.com>
2021-01-03 23:33:54 +0100

    dodano zadania - sortowanie rust

---
+++ src/Rust/22_Sortowanie_Praktyka/Zad1.rs
@@ -0,0 +1,26 @@
+use std::iter::Iterator;
+use std::iter::FromIterator;
+
+fn sort_v1(napis: String ) -> String {
+    let s_slice: &str = &napis[..];
+    let mut chars: Vec<char> = s_slice.chars().collect();
+    chars.sort_by(|a, b| a.cmp(b));
+    return String::from_iter(chars);
+}
+
+fn test_1() {
+	let napis: String = "cab".to_owned();
+	let wynik = "abc".to_owned();
+	assert_eq!(sort_v1(napis), wynik);
+}
+
+fn test_2() {
+	let napis: String = "éDedCBcbAàa".to_owned();
+	let wynik = "ABCDabcdeàé".to_owned();
+	assert_eq!(sort_v1(napis), wynik);
+}
+
+fn main() {
+	test_1();
+	test_2();
+}
