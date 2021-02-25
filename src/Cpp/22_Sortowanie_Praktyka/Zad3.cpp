b7f07601e1bc716bcb8aa7e8a5b99f7b9d8f1f0a
djeada <addjellouli1@gmail.com>
2021-02-25 09:53:31 +0100

    dodano zadania - sortowanie praktyka

---
+++ src/Cpp/22_Sortowanie_Praktyka/Zad3.cpp
@@ -0,0 +1,51 @@
+#include <string>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <cassert>
+
+//Otrzymujesz liste par napisow i liczb. Posortuj pary w liscie wzgledem:
+//a) wartosci liczb;
+void sortujWartosci (std::vector<std::pair<std::string, int>>& lista)
+{
+    std::sort(lista.begin(), lista.end(), [](auto rhs, auto lhs){
+		return rhs.second < lhs.second;
+	});
+}
+
+//b) dlugosci napisow;
+void sortujDlugosci (std::vector<std::pair<std::string, int>>& lista)
+{
+    std::sort(lista.begin(), lista.end(), [](auto rhs, auto lhs){
+		return rhs.first.size() < lhs.first.size();
+	});
+}
+
+
+void test1() 
+{
+	std::vector<std::pair<std::string, int>> lista {{"abc", 3}, {"bca", 1}, {"cab", 2}};
+	std::vector<std::pair<std::string, int>> wynik {{"bca", 1}, {"cab", 2}, {"abc", 3}};
+	
+	sortujWartosci(lista);
+
+	assert(lista == wynik);
+}
+
+void test2() 
+{
+	std::vector<std::pair<std::string, int>> lista {{"ab", 3}, {"abc", 1}, {"a", 2}};
+	std::vector<std::pair<std::string, int>> wynik {{"a", 2}, {"ab", 3}, {"abc", 1}};
+	
+	sortujDlugosci(lista);
+
+	assert(lista == wynik);
+}
+
+int main() {
+
+	test1();
+	test2();
+
+	return 0;
+}
