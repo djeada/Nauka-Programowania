750ae4295fe4dc226d96ae0fdd22fee3b04045b4
djeada <addjellouli1@gmail.com>
2021-03-09 23:10:31 +0100

    zmiana formatowania

--- src/Java/02_Instrukcje_sterujace/Zad7/Main.java
+++ src/Java/02_Instrukcje_sterujace/Zad7/Main.java
@@ -1,111 +1,108 @@
+package test;
+public class Main {
+    public static void main(String[] args) {
 
-public class Main
-{
-	public static void main(String[] args)
-	{
+        //Prawo wylacznego srodka
+        System.out.print("Prawo wylacznego srodka");
+        System.out.print(System.getProperty("line.separator"));
+        boolean p = false;
+        System.out.print("dla p majacego wartosc logiczna ");
+        System.out.print(p);
+        System.out.print(" wyrazenie p v ~p ma wartosc logiczna ");
+        System.out.printf("%b", (p || !p));
+        System.out.printf(System.getProperty("line.separator"));
+        p = true;
+        System.out.printf("dla p majacego wartosc logiczna ");
+        System.out.printf("%b", p);
+        System.out.printf(" wyrazenie p v ~p ma wartosc logiczna ");
+        System.out.printf("%b", (p || !p));
+        System.out.printf(System.getProperty("line.separator"));
 
+        //Zasada niesprzecznosci
+        System.out.printf("\nZasada niesprzecznosci");
+        System.out.printf(System.getProperty("line.separator"));
+        p = false;
+        System.out.printf("dla p majacego wartosc logiczna ");
+        System.out.printf("%b", p);
+        System.out.printf(" wyrazenie ~(p Ʌ ~p) ma wartosc logiczna ");
+        System.out.printf("%b", !(p && !p));
+        System.out.printf(System.getProperty("line.separator"));
+        p = true;
+        System.out.printf("dla p majacego wartosc logiczna ");
+        System.out.printf("%b", p);
+        System.out.printf(" wyrazenie ~(p Ʌ ~p) ma wartosc logiczna ");
+        System.out.printf("%b", !(p && !p));
+        System.out.printf(System.getProperty("line.separator"));
 
-		//Prawo wylacznego srodka
-		System.out.print("Prawo wylacznego srodka");
-		System.out.print("\n");
-		boolean p = false;
-		System.out.print("dla p majacego wartosc logiczna ");
-		System.out.print(p);
-		System.out.print(" wyrazenie p v ~p ma wartosc logiczna ");
-		System.out.printf("%b", (p || !p));
-		System.out.printf("%b", "\n");
-		p = true;
-		System.out.printf("%b", "dla p majacego wartosc logiczna ");
-		System.out.printf("%b", p);
-		System.out.printf("%b", " wyrazenie p v ~p ma wartosc logiczna ");
-		System.out.printf("%b", (p || !p));
-		System.out.printf("%b", "\n");
+        //Przemiennosc koniunkcji
+        System.out.printf("\nPrzemiennosc koniunkcji");
+        System.out.printf(System.getProperty("line.separator"));
+        p = false;
+        boolean q = false;
+        System.out.printf("dla p majacego wartosc logiczna ");
+        System.out.printf("%b", p);
+        System.out.printf(" oraz q majacego wartosc logiczna ");
+        System.out.printf("%b", q);
+        System.out.printf(" wyrazenie (p Ʌ q) < = > (q Ʌ p) ma wartosc logiczna ");
+        System.out.printf("%b", ((p && q) == (q && p)));
+        System.out.printf(System.getProperty("line.separator"));
+        p = true;
+        q = false;
+        System.out.printf("dla p majacego wartosc logiczna ");
+        System.out.printf("%b", p);
+        System.out.printf(" oraz q majacego wartosc logiczna ");
+        System.out.printf("%b", q);
+        System.out.printf(" wyrazenie (p Ʌ q) < = > (q Ʌ p) ma wartosc logiczna ");
+        System.out.printf("%b", ((p && q) == (q && p)));
+        System.out.printf(System.getProperty("line.separator"));
+        p = false;
+        q = true;
+        System.out.printf("dla p majacego wartosc logiczna ");
+        System.out.printf("%b", p);
+        System.out.printf(" oraz q majacego wartosc logiczna ");
+        System.out.printf("%b", q);
+        System.out.printf(" wyrazenie (p Ʌ q) < = > (q Ʌ p) ma wartosc logiczna ");
+        System.out.printf("%b", ((p && q) == (q && p)));
+        System.out.printf(System.getProperty("line.separator"));
+        p = true;
+        q = true;
+        System.out.printf("dla p majacego wartosc logiczna ");
+        System.out.printf("%b", p);
+        System.out.printf(" oraz q majacego wartosc logiczna ");
+        System.out.printf("%b", q);
+        System.out.printf(" wyrazenie (p Ʌ q) < = > (q Ʌ p) ma wartosc logiczna ");
+        System.out.printf("%b", ((p && q) == (q && p)));
+        System.out.printf(System.getProperty("line.separator"));
 
-		//Zasada niesprzecznosci
-		System.out.printf("%b", "\nZasada niesprzecznosci");
-		System.out.printf("%b", "\n");
-		p = false;
-		System.out.printf("%b", "dla p majacego wartosc logiczna ");
-		System.out.printf("%b", p);
-		System.out.printf("%b", " wyrazenie ~(p Ʌ ~p) ma wartosc logiczna ");
-		System.out.printf("%b", !(p && !p));
-		System.out.printf("%b", "\n");
-		p = true;
-		System.out.printf("%b", "dla p majacego wartosc logiczna ");
-		System.out.printf("%b", p);
-		System.out.printf("%b", " wyrazenie ~(p Ʌ ~p) ma wartosc logiczna ");
-		System.out.printf("%b", !(p && !p));
-		System.out.printf("%b", "\n");
-
-		//Przemiennosc koniunkcji
-		System.out.printf("%b", "\nPrzemiennosc koniunkcji");
-		System.out.printf("%b", "\n");
-		p = false;
-		boolean q = false;
-		System.out.printf("%b", "dla p majacego wartosc logiczna ");
-		System.out.printf("%b", p);
-		System.out.printf("%b", " oraz q majacego wartosc logiczna ");
-		System.out.printf("%b", q);
-		System.out.printf("%b", " wyrazenie (p Ʌ q) < = > (q Ʌ p) ma wartosc logiczna ");
-		System.out.printf("%b", ((p && q) == (q && p)));
-		System.out.printf("%b", "\n");
-		p = true;
-		q = false;
-		System.out.printf("%b", "dla p majacego wartosc logiczna ");
-		System.out.printf("%b", p);
-		System.out.printf("%b", " oraz q majacego wartosc logiczna ");
-		System.out.printf("%b", q);
-		System.out.printf("%b", " wyrazenie (p Ʌ q) < = > (q Ʌ p) ma wartosc logiczna ");
-		System.out.printf("%b", ((p && q) == (q && p)));
-		System.out.printf("%b", "\n");
-		p = false;
-		q = true;
-		System.out.printf("%b", "dla p majacego wartosc logiczna ");
-		System.out.printf("%b", p);
-		System.out.printf("%b", " oraz q majacego wartosc logiczna ");
-		System.out.printf("%b", q);
-		System.out.printf("%b", " wyrazenie (p Ʌ q) < = > (q Ʌ p) ma wartosc logiczna ");
-		System.out.printf("%b", ((p && q) == (q && p)));
-		System.out.printf("%b", "\n");
-		p = true;
-		q = true;
-		System.out.printf("%b", "dla p majacego wartosc logiczna ");
-		System.out.printf("%b", p);
-		System.out.printf("%b", " oraz q majacego wartosc logiczna ");
-		System.out.printf("%b", q);
-		System.out.printf("%b", " wyrazenie (p Ʌ q) < = > (q Ʌ p) ma wartosc logiczna ");
-		System.out.printf("%b", ((p && q) == (q && p)));
-		System.out.printf("%b", "\n");
-
-		//Przemiennosc alternatywy
-		System.out.printf("%b", "\nPrzemiennosc alternatywy");
-		System.out.printf("%b", "\n");
-		p = false;
-		q = false;
-		System.out.printf("%b", "dla p majacego wartosc logiczna ");
-		System.out.printf("%b", p);
-		System.out.printf("%b", " oraz q majacego wartosc logiczna ");
-		System.out.printf("%b", q);
-		System.out.printf("%b", " wyrazenie (p v q) < = > (q v p) ma wartosc logiczna ");
-		System.out.printf("%b", ((p || q) == (q || p)));
-		System.out.printf("%b", "\n");
-		p = true;
-		q = false;
-		System.out.printf("%b", "dla p majacego wartosc logiczna ");
-		System.out.printf("%b", p);
-		System.out.printf("%b", " oraz q majacego wartosc logiczna ");
-		System.out.printf("%b", q);
-		System.out.printf("%b", " wyrazenie (p v q) < = > (q v p) ma wartosc logiczna ");
-		System.out.printf("%b", ((p || q) == (q || p)));
-		System.out.printf("%b", "\n");
-		p = false;
-		q = true;
-		System.out.printf("%b", "dla p majacego wartosc logiczna ");
-		System.out.printf("%b", p);
-		System.out.printf("%b", " oraz q majacego wartosc logiczna ");
-		System.out.printf("%b", q);
-		System.out.printf("%b", " wyrazenie (p v q) < = > (q v p) ma wartosc logiczna ");
-		System.out.printf("%b", ((p || q) == (q || p)));
-		System.out.printf("%b", "\n");
-	}
+        //Przemiennosc alternatywy
+        System.out.printf("\nPrzemiennosc alternatywy");
+        System.out.printf(System.getProperty("line.separator"));
+        p = false;
+        q = false;
+        System.out.printf("dla p majacego wartosc logiczna ");
+        System.out.printf("%b", p);
+        System.out.printf(" oraz q majacego wartosc logiczna ");
+        System.out.printf("%b", q);
+        System.out.printf(" wyrazenie (p v q) < = > (q v p) ma wartosc logiczna ");
+        System.out.printf("%b", ((p || q) == (q || p)));
+        System.out.printf(System.getProperty("line.separator"));
+        p = true;
+        q = false;
+        System.out.printf("dla p majacego wartosc logiczna ");
+        System.out.printf("%b", p);
+        System.out.printf(" oraz q majacego wartosc logiczna ");
+        System.out.printf("%b", q);
+        System.out.printf(" wyrazenie (p v q) < = > (q v p) ma wartosc logiczna ");
+        System.out.printf("%b", ((p || q) == (q || p)));
+        System.out.printf(System.getProperty("line.separator"));
+        p = false;
+        q = true;
+        System.out.printf("dla p majacego wartosc logiczna ");
+        System.out.printf("%b", p);
+        System.out.printf(" oraz q majacego wartosc logiczna ");
+        System.out.printf("%b", q);
+        System.out.printf(" wyrazenie (p v q) < = > (q v p) ma wartosc logiczna ");
+        System.out.printf("%b", ((p || q) == (q || p)));
+        System.out.printf(System.getProperty("line.separator"));
+    }
 }
