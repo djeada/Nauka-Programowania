173fe30e945df4769bf17fbd361436550b9076e8
djeada <addjellouli1@gmail.com>
2021-03-09 21:18:18 +0100

    zmiana formatowania

--- src/Java/01_Interakcja_z_konsola/Zad1/Main.java
+++ src/Java/01_Interakcja_z_konsola/Zad1/Main.java
@@ -1,5 +1,3 @@
-package <missing>;
-
 public class Main
 {
 	public static void main(String[] args)
@@ -9,15 +7,13 @@ public class Main
 		Wypisz na konsole swoje inicjaly.
 		*/
 
-		System.out.print("A.D.");
-		System.out.print("\n");
+		System.out.print("A.D. \n");
 
 		/*
 		Wypisz na konsole "witaj swiecie"
 		*/
 
-		System.out.print("Witaj swiecie");
-		System.out.print("\n");
+		System.out.print("Witaj swiecie\n");
 
 	}
 
