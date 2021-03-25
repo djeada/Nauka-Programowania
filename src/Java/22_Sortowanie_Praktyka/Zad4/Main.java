fe455366d53062c31a2e76b85b83e58e6c6b8d6b
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2021-03-26 00:06:26 +0100

    formatowanie

--- src/Java/22_Sortowanie_Praktyka/Zad4/Main.java
+++ src/Java/22_Sortowanie_Praktyka/Zad4/Main.java
@@ -6,42 +6,48 @@ Posortuj tablice napisow wzgledem dlugosci.
 */
 
 public class Main {
-	
 
-    public static void sortujWzgledemDlugosciV1(String[] napisy) {
+	public static void sortujWzgledemDlugosciV1(String[] napisy) {
 
-        Arrays.sort(napisy, (String s1, String s2)
-                    -> (-1) * Integer.compare(s1.length(), s2.length()));
-    }
+		Arrays.sort(napisy, (String s1, String s2) -
+			> (-1) * Integer.compare(s1.length(), s2.length()));
+	}
 
-    public static void sortujWzgledemDlugosciV2(String[] napisy) {
-    	
-        Arrays.sort(napisy, Comparator.comparingInt(String::length).reversed());
-    
-    }
+	public static void sortujWzgledemDlugosciV2(String[] napisy) {
 
-    public static String[] sortujWzgledemDlugosciV3(String[] napisy) {
+		Arrays.sort(napisy, Comparator.comparingInt(String::length).reversed());
 
-            return Arrays.stream(napisy)
-                    .sorted(Comparator.comparingInt(String::length).reversed())
-                    .toArray(String[]::new);
-    }
-    
+	}
 
-    public static void main(String[] args) {
-    	
-    	String[] napisy = new String[]{"Lezy", "jerzy", "na", "wiezyczce"};
-    	String[] wynik = new String[]{"wiezyczce", "jerzy", "Lezy", "na"};
+	public static String[] sortujWzgledemDlugosciV3(String[] napisy) {
 
-    	sortujWzgledemDlugosciV1(napisy);
-    	assert Arrays.equals(napisy, wynik);
-    	
-    	napisy = new String[]{"Lezy", "jerzy", "na", "wiezyczce"};
-    	sortujWzgledemDlugosciV2(napisy);
-    	assert Arrays.equals(napisy, wynik);
+		return Arrays.stream(napisy)
+			.sorted(Comparator.comparingInt(String::length).reversed())
+			.toArray(String[]::new);
+	}
 
-    	napisy = new String[]{"Lezy", "jerzy", "na", "wiezyczce"};
-    	assert Arrays.equals(sortujWzgledemDlugosciV3(napisy), wynik);
-    }
+	public static void main(String[] args) {
+
+		String[] napisy = new String[] {
+			"Lezy", "jerzy", "na", "wiezyczce"
+		};
+		String[] wynik = new String[] {
+			"wiezyczce", "jerzy", "Lezy", "na"
+		};
+
+		sortujWzgledemDlugosciV1(napisy);
+		assert Arrays.equals(napisy, wynik);
+
+		napisy = new String[] {
+			"Lezy", "jerzy", "na", "wiezyczce"
+		};
+		sortujWzgledemDlugosciV2(napisy);
+		assert Arrays.equals(napisy, wynik);
+
+		napisy = new String[] {
+			"Lezy", "jerzy", "na", "wiezyczce"
+		};
+		assert Arrays.equals(sortujWzgledemDlugosciV3(napisy), wynik);
+	}
 
 }
