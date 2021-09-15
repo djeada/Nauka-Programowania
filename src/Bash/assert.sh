4635d22154eeb57534a853a24266e84d6438476e
djeada <addjellouli1@gmail.com>
2021-09-15 21:19:38 +0200

    poszerzono framework to testow jednostkowych

--- src/Bash/assert.sh
+++ src/Bash/assert.sh
@@ -1,42 +1,76 @@
-assert () {                     
-	E_ASSERT_FAILED=99
-	E_PARAM_ERR=98
+E_ASSERT_FAILED=99
+E_PARAM_ERR=98
 
-	if [ -z "$2" ]; then
-		return $E_PARAM_ERR
-	fi
 
-	lineno=$2
+assert () {
+    if [ -z "$2" ]; then
+        return $E_PARAM_ERR
+    fi
 
-	if [ ! $1 ]; then
-		echo "Assertion failed:  \"$1\""
-		echo "File \"$0\", line $lineno"
-		exit $E_ASSERT_FAILED
-	fi
-}  
+    local lineno=$2
 
-assertTrue () {
-	E_ASSERT_FAILED=99
-	E_PARAM_ERR=98
-	lineno=$2
+    if [ ! $1 ]; then
+        echo "Assertion failed:  \"$1\""
+        echo "File \"$0\", line $lineno"
+        exit $E_ASSERT_FAILED
+    fi
+}
 
-	if [ "$1" = false ]; then
-		echo "Assertion failed:  \"$1\""
-		echo "File \"$0\", line $lineno"
-		exit $E_ASSERT_FAILED
-	fi
+assertEqual () {
+    local lineno=$3
+
+    if [ $1 -ne $2 ]; then
+        echo "Assertion failed:  \"$1\""
+        echo "File \"$0\", line $lineno"
+        exit $E_ASSERT_FAILED
+    fi
+}
+
+assertNotEqual () {
+    local lineno=$3
+
+    if [ $1 -eq $2 ]; then
+        echo "Assertion failed:  \"$1\""
+        echo "File \"$0\", line $lineno"
+        exit $E_ASSERT_FAILED
+    fi
+}
+
+assertArrayEqual() {
+    local -n array_a=$1
+    local -n array_b=$2
+    local lineno=$3
+
+    #check length equal
+    n=$(( ${#array_a[@]}))
+    m=$(( ${#array_b[@]}))
+
+    assertEqual $n $m $lineno
+
+    #check element for element
+    for (( i=0; i<n; i++ ))
+    do
+        assertEqual ${array_a[$i]} ${array_b[$i]} $lineno
+    done
 
 }
 
+assertTrue () {
+    local lineno=$2
+
+    if [ "$1" = false ]; then
+        echo "Assertion failed:  \"$1\""
+        echo "File \"$0\", line $lineno"
+        exit $E_ASSERT_FAILED
+    fi
+}
+
 assertFalse () {
-	E_ASSERT_FAILED=99
-	E_PARAM_ERR=98
-	lineno=$2
+    local lineno=$2
 
-	if [ "$1" = true ]; then
-		echo "Assertion failed:  \"$1\""
-		echo "File \"$0\", line $lineno"
-		exit $E_ASSERT_FAILED
-	fi
-
+    if [ "$1" = true ]; then
+        echo "Assertion failed:  \"$1\""
+        echo "File \"$0\", line $lineno"
+        exit $E_ASSERT_FAILED
+    fi
 }
