07b8be3d50ab9601f60a9d265c8979f1ca85789b
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2021-10-31 01:39:31 +0200

    poprawiono literowki

--- src/Python/01_Interakcja_z_konsola/Zad6.py
+++ src/Python/01_Interakcja_z_konsola/Zad6.py
@@ -6,7 +6,7 @@ b) Pobierz wielkość w calach i wypisz ilu centymetrom odpowiada.
 c) Pobierz liczbę sekund i przelicz na godziny.
 d) Pobierz liczbę w euro i wypisz ilu złotówkom odpowiada. 
 e) Pobierz miarę kąta w stopniach i wypisz ilu radianom odpowiada.
-f) Pobierz temperaturę w stopniach Farenheita i wypisz ilu stopniom Celsjusza oraz ilu stopniom Kelwina odpowiada. 
+f) Pobierz temperaturę w stopniach Fahrenheita i wypisz ilu stopniom Celsjusza oraz ilu stopniom Kelwina odpowiada. 
 """
 
 if __name__ == "__main__":
@@ -46,7 +46,7 @@ if __name__ == "__main__":
 
     print(kat_stopnie, " stopni to ", kat_rad, " radianow")
 
-    print("podaj temperature w Farenheitach:")
+    print("podaj temperature w Fahrenheitach:")
 
     temp_f = int(input())
     temp_c = (temp_f - 32) * 5 / 9
