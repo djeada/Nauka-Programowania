7568ee23fba7424072e0be886054f9b1c6008b07
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2021-10-31 01:39:02 +0200

    poprawiono literowki

--- src/Cpp/01_Interakcja_z_konsola/Zad6.cpp
+++ src/Cpp/01_Interakcja_z_konsola/Zad6.cpp
@@ -8,7 +8,7 @@ b) Pobierz wielkość w calach i wypisz ilu centymetrom odpowiada.
 c) Pobierz liczbę sekund i przelicz na godziny.
 d) Pobierz liczbę w euro i wypisz ilu złotówkom odpowiada. 
 e) Pobierz miarę kąta w stopniach i wypisz ilu radianom odpowiada.
-f) Pobierz temperaturę w stopniach Farenheita i wypisz ilu stopniom Celsjusza oraz ilu stopniom Kelwina odpowiada. 
+f) Pobierz temperaturę w stopniach Fahrenheita i wypisz ilu stopniom Celsjusza oraz ilu stopniom Kelwina odpowiada. 
 */
 
 int main() {
