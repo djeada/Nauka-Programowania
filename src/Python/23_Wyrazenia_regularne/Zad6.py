61d4e6630279690c681fa7327c4b2522fe1c8b56
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2021-12-05 19:42:08 +0100

    Poprawiono rozwiazanie [ Python/23/Zad6 ]

--- src/Python/23_Wyrazenia_regularne/Zad6.py
+++ src/Python/23_Wyrazenia_regularne/Zad6.py
@@ -7,36 +7,23 @@ dowolnym znakiem interpunkcyjnym.
 import re
 
 
-def zaczyna_sie_lub_konczy(tekst, napis_a, napis_b):
+def wiersze_konczace_sie_napisem(tekst, koniec):
     """
-    Funkcja zwraca True jesli tekst zaczyna sie lub konczy napis_a
-    lub napis_b.
+    Zwraca wszystkie wiersze konczace sie napisem koniec.
     """
-    return [
-        wiersz
-        for wiersz in tekst.splitlines()
-        if re.search(r"^{0}".format(napis_a), wiersz, flags=re.M)
-           or re.search(r"{0}[!?.,;]$".format(napis_b), wiersz, flags=re.M)
-    ]
+    wiersze = re.split("\n|\.|\?|\!|\;|\:", tekst)
+    return [wiersz for wiersz in wiersze if wiersz.endswith(koniec)]
 
 
-def test_zaczyna_sie_lub_konczy():
+def test_wiersze_konczace_sie_napisem():
     tekst = """Folgujmy paniom nie sobie, ma rada;
 Milujmy wiernie nie jest w nich przysada.
 Godnosci trzeba nie za nic tu cnota,
 Milosci pragna nie pragna tu zlota."""
+    napis = "da"
+    oczekiwane=["Folgujmy paniom nie sobie, ma rada", "Milujmy wiernie nie jest w nich przysada"]
 
-    napis_a = "Mi"
-    napis_b = "da"
-
-    oczekiwane = [
-        "Folgujmy paniom nie sobie, ma rada;",
-        "Milujmy wiernie nie jest w nich przysada.",
-        "Milosci pragna nie pragna tu zlota.",
-    ]
-
-    assert zaczyna_sie_lub_konczy(tekst, napis_a, napis_b) == oczekiwane
-
+    assert wiersze_konczace_sie_napisem(tekst, napis) == oczekiwane
 
 if __name__ == "__main__":
-    test_zaczyna_sie_lub_konczy()
+    test_wiersze_konczace_sie_napisem()
